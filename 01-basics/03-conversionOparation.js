fd415f65ccfd82a68c006d9f3cea789135006963
Sipankar62 <sipankardebnath@gmail.com>
2024-08-13 03:56:01 +0000

    data types and it's conversion

---
+++ 01-basics/03-conversionOparation.js
@@ -0,0 +1,26 @@
+let score = "33gfyjgv"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "355abvb" => NaN
+// true => 1; false => 0
+
+let isloggedIn ="sipankar"
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn)
+// 1 =>true; 0 => false
+//"" => false
+// "sipankar" => true
+
+let number = 56
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber)
