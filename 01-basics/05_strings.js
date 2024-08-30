341039fed4c060efcfc4ffceeeadfefaa2f9cb47
Sipankar62 <sipankardebnath@gmail.com>
2024-08-30 17:31:33 +0000

    strings

---
+++ 01-basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "Sipankar Debnath"
+const repoCount = 40
+
+// console.log(name + repoCount +"DJ")
+
+ // console.log( `Hello my name is ${name} and my repo count is ${repoCount}`);
+
+ const myBranch = new String('MCA at giet ghangapatna')
+
+
+ console.log(myBranch)
+ console.log(myBranch.length)
+ console.log(myBranch.toLowerCase());
+ console.log(myBranch.charAt(2));
+ console.log(myBranch.indexOf('C'))
+
+ const newString = myBranch.substring(0,6)
+ console.log(newString)
+
+ const anotherString = myBranch.slice(-5,-2)
+ console.log(anotherString)
+
+ const newStringOne = "    BHubneswar   "
+ console.log(newStringOne)
+ console.log(newStringOne.trim())  //  basically trim is used for remove the space 
+
+ const url = "https://linked.in/sipankar@62debnath"
+
+ console.log(url.replace('@62','_'))  //for Replace word
+
+ console.log(url.includes('ashim'))
+
+ console.log(myBranch.split(' '))
+
+
