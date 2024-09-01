f8cfceb85cce821823c9a506d3080cd448639d3e
Sipankar62 <sipankardebnath@gmail.com>
2024-09-01 14:42:13 +0000

    strings

---
+++ 01-basics/06-nums_maths.js
@@ -0,0 +1,36 @@
+const balance = 100
+//console.log(balance);
+
+const score = new Number(500.)
+//console.log(score)
+
+//console.log(score.toString());
+//console.log(score.toString().length);
+//console.log(score.toFixed(2));
+
+const otherNumber = 5835.6555
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// *****************************  MATHS  *******************
+
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(5.9));
+//console.log(Math.ceil(8.1));
+//console.log(Math.floor(9.6));
+//console.log(Math.min(7,8,6,3));
+//console.log(Math.max(7,8,6,3));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
